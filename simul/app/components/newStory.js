36d2b955af5d18e928aabfb25f59d2e432b53c50
hypatiah <danielle.hassid@gmail.com>
2016-09-03 21:58:55 -0700

    Add newStory skeleton

---
+++ simul/app/components/newStory.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+class NewStory extends Component{
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    )
+  }
+};
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+   fontSize: 20,
+   alignSelf: 'center',
+   margin: 40
+  },
+});
+
+module.exports = NewStory;
