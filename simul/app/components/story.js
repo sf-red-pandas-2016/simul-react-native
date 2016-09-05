dff7394f70c8a934abd037e955c8c93f7a8e3ad2
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-05 09:51:23 -0700

    Add Touchable Highlight for onProfilePressed with onProfilePressed function

--- simul/app/components/story.js
+++ simul/app/components/story.js
@@ -4,16 +4,35 @@ import {
   StyleSheet,
   Text,
   View,
+  TouchableHighlight,
 } from 'react-native';
 
 class Story extends Component{
+
+  onProfilePressed() {
+    this.props.navigator.push({
+      title: 'Story',
+      component: Story
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Day 7</Text>
           <Text style={styles.title}> My neighbor Amira </Text>
           <Text style={styles.content}> Today I met another girl my age named Amira, her family is like mine...</Text>
+
+
+      <TouchableHighlight onPress={this.onProfilePressed.bind(this)} style={styles.button}>
+        <Text style={styles.buttonText}>
+        Ahmeds Profile
+        </Text>
+      </TouchableHighlight>
+
       </View>
+
+
     )
   }
 };
