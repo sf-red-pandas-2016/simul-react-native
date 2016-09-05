65d407842910848af6a5785f8b85cf727a2f5450
hypatiah <danielle.hassid@gmail.com>
2016-09-04 20:45:43 -0700

    Create Search component

---
+++ simul/app/components/search.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'powderblue',
+  },
+  input: {
+    height: 30,
+    flex: 1,
+    paddingHorizontal: 8,
+    fontSize: 15,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 2,
+  },
+});
+
+const Search = (props) => (
+  <View style={styles.container}>
+    <TextInput
+      style={styles.input}
+      placeholder="Search..."
+      onChangeText={(text) => console.log('searching for ', text)}
+    />
+  </View>
+);
+
+module.exports = Search;
