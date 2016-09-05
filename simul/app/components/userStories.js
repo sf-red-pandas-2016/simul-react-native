f9f3b8e6e8686a896ea1cc7b99654928ef47a11d
hypatiah <danielle.hassid@gmail.com>
2016-09-04 22:39:50 -0700

    Add ListView data, and nav to Story to UserStories

--- simul/app/components/userStories.js
+++ simul/app/components/userStories.js
@@ -9,6 +9,32 @@ import {
 } from 'react-native';
 
 class UserStories extends Component{
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        'Story 1', 'Story 2', 'Story 3', 'Story 4', 'Story 5', 'Story 6', 'Story 7', 'Story 8', 'Story 9', 'Story 10', 'Story 11'
+      ])
+    };
+  }
+
+  _onPressStory() {
+    this.props.navigator.push({
+      title: 'Story',
+      component: Story
+    })
+  }
+
+  newestStory() {
+    return(
+      <View style={{backgroundColor: 'lightgrey'}}>
+        <Text style={{color: 'purple', textAlign: 'left'}}>Monday August 24, 2016</Text>
+        <Text>"My day today was very interesting. First I woke up late and I couldn't find my clean clothes and my mom......"</Text>
+        <Text>كان يوم لي اليوم مثيرة جدا للاهتمام. أولا استيقظت في وقت متأخر، وأنا لا يمكن أن تجد لي ملابس نظيفة وأمي</Text>
+      </View>
+    )
+  }
   render() {
     return (
       <View style={styles.container}>
