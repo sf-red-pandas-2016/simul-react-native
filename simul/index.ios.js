31b80d5393e39acc2911e5b72e5f8ed32abc7b33
hypatiah <danielle.hassid@gmail.com>
2016-09-05 17:30:06 -0700

    Clean up unnecessary comments/spacing in index

--- simul/index.ios.js
+++ simul/index.ios.js
@@ -53,30 +53,10 @@ I18n.translations = {
     profile: 'الملف الشخصي',
     message: 'الرسالة',
     messages: 'رسائل',
-
-
   }
 }
 
-
-
-
 import Enter from './app/components/enter.js';
-// ======= OLD NAV
-// import Enter from './app/components/enter.js';
-// import Home from './app/components/home.js';
-// import Contact from './app/components/contact.js';
-// import Login from './app/components/login.js';
-// import NewStory from './app/components/newStory.js';
-// import Profile from './app/components/profile.js';
-// import Register from './app/components/register.js';
-// import Story from './app/components/story.js';
-// import UserStories from './app/components/userStories.js';
-// import UserMessages from './app/components/userMessages.js';
-// import Message from './app/components/message.js';
-// import Nav from './app/components/nav.js'
-
-
 
 class simul extends Component {
   render() {
@@ -88,48 +68,6 @@ class simul extends Component {
         }}
         style={{flex: 1}}
         />
-      // ======= OLD NAV
-//       <Navigator
-//         initialRoute={routes[2]}
-//         initialRouteStack={routes}
-//         renderScene={(route, navigator) => {
-//           if(route.title == 'Enter') {
-//            return <Enter title={route.title} />
-//           }
-//           if(route.title == 'Home') {
-//            return <Home title={route.title} />
-//           }
-//           if(route.title == 'Profile') {
-//            return <Profile title={route.title} />
-//           }
-//           if(route.title == 'UserStories') {
-//            return <UserStories title={route.title} />
-//           }
-//           if(route.title == 'Register') {
-//            return <Register title={route.title} />
-//           }
-//           if(route.title == 'Login') {
-//            return <Login title={route.title} />
-//           }
-//           if(route.title == 'Story') {
-//            return <Story title={route.title} />
-//           }
-//           if(route.title == 'NewStory') {
-//            return <NewStory title={route.title} />
-//           }
-//           if(route.title == 'Contact') {
-//            return <Contact title={route.title} />
-//           }
-//           if(route.title == 'UserMessages') {
-//            return <UserMessages title={route.title} />
-//           }
-//           if(route.title == 'Message') {
-//            return <Message title={route.title} />
-//           }
-//          if(route.title == 'Nav'){
-//            return <Nav title={route.title} />
-//          }
-//
     );
   }
 }
