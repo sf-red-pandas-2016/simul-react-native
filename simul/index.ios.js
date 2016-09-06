c385fa4bdd049d22c242a0fce8f21f6412ed8d83
hypatiah <danielle.hassid@gmail.com>
2016-09-05 17:29:06 -0700

    Create i18n.translations array for en and ar

--- simul/index.ios.js
+++ simul/index.ios.js
@@ -7,6 +7,59 @@ import {
   NavigatorIOS,
 } from 'react-native';
 
+import I18n from 'react-native-i18n'
+
+I18n.fallbacks = true;
+
+I18n.translations = {
+  en: {
+    login: 'Login',
+    enter: 'Enter',
+    register: 'Register',
+    home: 'Home',
+    username: 'Username',
+    password: 'Password',
+    name: 'Name',
+    location: 'Location',
+    bio: 'Bio',
+    skills: 'Skills/Expertise',
+    contactInformation: 'Contact Information',
+    resourceRequest: 'Resource Request',
+    seeking: 'Opportunities Seeking',
+    search: 'Search',
+    story: 'Story',
+    stories: 'Stories',
+    message: 'Message',
+    messages: 'Messages',
+    profile: 'Profile',
+  },
+  ar: {
+    login: 'دخول',
+    username: 'اسم المستخدم',
+    password: 'كلمه السر',
+    name: 'اسم',
+    enter: 'أدخل',
+    register: 'تسجيل',
+    home: 'منزل',
+    bio: 'سيرة',
+    location: 'موقع',
+    contactInformation: 'معلومات الاتصال',
+    skills: 'مهارات',
+    seeking: 'فرص تسعى',
+    resourceRequest: 'الموارد أريد',
+    search: 'بحث',
+    story: 'قصة',
+    stories: 'قصص',
+    profile: 'الملف الشخصي',
+    message: 'الرسالة',
+    messages: 'رسائل',
+
+
+  }
+}
+
+
+
 
 import Enter from './app/components/enter.js';
 // ======= OLD NAV
