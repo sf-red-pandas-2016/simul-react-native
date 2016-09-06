9c57c3fb29dde2c5b52ee5379e6002d1437c2834
hypatiah <danielle.hassid@gmail.com>
2016-09-05 18:26:44 -0700

    Clean up Message view, remove merge conflict arrows and spacing

--- simul/app/components/message.js
+++ simul/app/components/message.js
@@ -10,15 +10,12 @@ class Message extends Component{
   render() {
     return (
       <View style={styles.container}>
-<<<<<<< HEAD
+        <Text>Message</Text>
         <Text style={styles.to}>Jim Smith</Text>
         <Text style={styles.from}>from: Tom Smith</Text>
         <Text style={styles.from}>date: 08/04/2016</Text>
         <Text style={styles.title}>Europe</Text>
         <Text style={styles.content}>Europe is close to limit on accepting refugees, EU President Donald Tusk said Sunday, as he urged the international community to do more to step up resettlement of those seeking refuge.</Text>
-=======
-        <Text>Message</Text>
->>>>>>> badd7c5cad51a1bd08a6506271baab7bb799b44f
       </View>
     )
   }
@@ -50,7 +47,6 @@ var styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 60,
   },
-
   content: {
     alignSelf: 'center',
     fontSize: 21,
