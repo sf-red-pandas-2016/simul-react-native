065cb45c78ad75a4c171f35352a9f46c13c584b2
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-06 09:45:32 -0700

    Add onPressAddStory function to profile.js

--- simul/app/components/profile.js
+++ simul/app/components/profile.js
@@ -4,16 +4,29 @@ import {
   StyleSheet,
   Text,
   View,
+  TouchableHighlight,
 } from 'react-native';
 
 import I18n from 'react-native-i18n'
 
 class Profile extends Component{
+
+
+  async _onPressAddStory(){
+    try {
+      
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Smeagles {I18n.t('profile')}</Text>
-        <Text> {JSON.stringify(this.props.users)}</Text>
+        <Text style={styles.body}> {JSON.stringify(this.props.users)}</Text>
+        <Text style={styles.body}> Temporary for styling Skeleton </Text>
+        <TouchableHighlight onPress={this._onPressAddStory}.bind(this)} style={styles.button}>
+        <Text style={styles.buttonText}>
+        </Text>
+        </TouchableHighlight>
       </View>
     )
   }
@@ -29,8 +42,11 @@ var styles = StyleSheet.create({
    marginTop: 25,
    fontSize: 20,
    alignSelf: 'center',
-   margin: 40
+   margin: 40,
   },
+  body: {
+    flex: 0.1,
+  }
 });
 
 module.exports = Profile;
