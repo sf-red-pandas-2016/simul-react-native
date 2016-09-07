6ebbffa77a03a576a8f6640b98a8cb408909cccf
hypatiah <danielle.hassid@gmail.com>
2016-09-06 23:03:04 -0700

    Build basic skeleton for imageUpload

---
+++ simul/app/components/imageUpload.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import I18n from 'react-native-i18n'
+
+class imageUpload extends Component{
+  render() {
+    return (
+    )
+  }
+};
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#27c2dc'
+  },
+});
+module.exports = imageUpload;
