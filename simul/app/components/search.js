6483fc5f0337b53009b90668d24395564e049508
justinbryeans <justinbryeans@gmail.com>
2016-09-07 16:27:12 -0700

    Add isLoading to initial state, creating executeQuery function, add styling and TouchableHighlight to search bar and button

--- simul/app/components/search.js
+++ simul/app/components/search.js
@@ -19,9 +19,14 @@ class Search extends Component {
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
       searchString: '',
+      isLoading: false,
       stories: '?',
       dataSource: ds.cloneWithRows([]),
     };
+  }
+
+  componentDidMount() {
+      this.fetchData().done()
   }
 
   onSearchTextChanged(text) {
@@ -30,8 +35,9 @@ class Search extends Component {
   console.log(this.state.searchString);
   }
 
-  componentDidMount() {
-      this.fetchData().done()
+  executeQuery(searchString) {
+  console.log(searchString);
+  this.setState({ isLoading: true });
   }
 
   async fetchData() {
@@ -43,9 +49,9 @@ class Search extends Component {
       this.setState({dataSource: ds.cloneWithRows(stories),
       stories: stories})
 
-      if (stories[0].id === 1) {
-        console.log('match');
-      }
+      // if (stories[0].id === 1) {
+      //   console.log('match');
+      // }
 
   }
 
@@ -55,9 +61,12 @@ class Search extends Component {
       <View style={styles.container}>
           <TextInput
             style={styles.input}
-            placeholder={I18n.t('search')}
+            placeholder={I18n.t('Search')}
             onChangeText={this.onSearchTextChanged.bind(this)}
           />
+          <TouchableHighlight style={styles.button} onPress={ () => this.executeQuery()}>
+            <Text style={styles.buttonText}>Go</Text>
+          </TouchableHighlight>
         </View>
 
       // <View style={styles.container}>
@@ -81,15 +90,41 @@ const styles = StyleSheet.create({
     padding: 8,
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#27c2dc',
+    // backgroundColor: '#27c2dc',
   },
   input: {
-    height: 30,
-    flex: 1,
-    paddingHorizontal: 8,
-    fontSize: 15,
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#27C2DC',
+    borderRadius: 8,
+    color: '#48BBEC',
+    // height: 30,
+    // flex: 1,
+    // paddingHorizontal: 8,
+    // fontSize: 15,
     backgroundColor: '#FFFFFF',
-    borderRadius: 2,
+    // borderRadius: 2,
+  },
+  buttonText: {
+  fontSize: 18,
+  color: 'white',
+  alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#27C2DC',
+    borderColor: '#27C2DC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
   },
 });
 
