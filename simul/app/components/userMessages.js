c1627ae20976fe34e2fec64d2db9b8d8b59e6aaf
hypatiah <danielle.hassid@gmail.com>
2016-09-08 15:48:46 -0700

    Add scroll functionality to userMessages

--- simul/app/components/userMessages.js
+++ simul/app/components/userMessages.js
@@ -4,36 +4,43 @@ import {
   StyleSheet,
   Text,
   View,
+  ScrollView,
 } from 'react-native';
 
 class UserMessages  extends Component{
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.to}>Jim Smith</Text>
-        <Text style={styles.from}>from: Tom Smith</Text>
-        <Text style={styles.from}>date: 08/04/2016</Text>
-        <Text style={styles.title}>Europe</Text>
-        <Text style={styles.from}>from: Tom Smith</Text>
-        <Text style={styles.from}>date: 08/03/2016</Text>
-        <Text style={styles.title}>Another chance</Text>
-        <Text style={styles.from}>from: Tom Smith</Text>
-        <Text style={styles.from}>date: 08/02/2016</Text>
-        <Text style={styles.title}>We leave</Text>
-        <Text style={styles.from}>from: Tom Smith</Text>
-        <Text style={styles.from}>date: 08/01/2016</Text>
-        <Text style={styles.title}>No more food</Text>
-      </View>
+      <ScrollView style={styles.superContainer}>
+        <View style={styles.container}>
+          <Text style={styles.to}>Jim Smith</Text>
+          <Text style={styles.from}>from: Tom Smith</Text>
+          <Text style={styles.from}>date: 08/04/2016</Text>
+          <Text style={styles.title}>Europe</Text>
+          <Text style={styles.from}>from: Tom Smith</Text>
+          <Text style={styles.from}>date: 08/03/2016</Text>
+          <Text style={styles.title}>Another chance</Text>
+          <Text style={styles.from}>from: Tom Smith</Text>
+          <Text style={styles.from}>date: 08/02/2016</Text>
+          <Text style={styles.title}>We leave</Text>
+          <Text style={styles.from}>from: Tom Smith</Text>
+          <Text style={styles.from}>date: 08/01/2016</Text>
+          <Text style={styles.title}>No more food</Text>
+        </View>
+      </ScrollView>
     )
   }
 };
 
 var styles = StyleSheet.create({
+  superContainer: {
+    flex: 1,
+    backgroundColor: '#27c2dc',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#27c2dc'
+    backgroundColor: '#27c2dc',
   },
   title: {
    fontSize: 40,
